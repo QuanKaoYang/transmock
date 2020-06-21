2d7ebd2f669f75b579a06a218d85c0efdcf119b8
QuanKaoYang <ishida.goldenbridge2002@gmail.com>
2020-06-21 18:48:32 +0900

    primary done

--- docs/index.js
+++ docs/index.js
@@ -1,46 +1,3 @@
-const getTrans = (text) => {
-    return new Promise(resolve => {
-        fetch('https://sheepy-meme.builtwithdark.com/mock', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'text/plain',
-                'Access-Control-Allow-Origin':'*'
-            },
-            mode: 'cors',
-            body: text
-        }).then(res => {
-            if (res.statsu === 200) {
-                res.text().then(t => {
-                    resolve('translated')
-                })
-            }
-        })
-    })
-}
-
-async function submit2() {
-    console.log('here')
-    return new Promise((resolve, reject) => {
-        fetch(`https://sheepy-meme.builtwithdark.com/meebaalogin`, {
-            method: 'POST',
-            headers: {
-                    'Content-Type': 'text/plain',
-                },
-            mode: 'cors',
-            body: 'hoge',
-        }).then(res => {
-            if (res.status === 200) {
-                res.text().then(t => {
-                    resolve(t);
-                })
-            } else {
-                reject("Login Failed");
-            }
-        }).catch(() => {
-            reject("Login Failed")
-        })
-    })
-}
 const vm = new Vue({
     el: '#app',
     data: {
@@ -49,8 +6,23 @@ const vm = new Vue({
     },
     methods: {
         async submitSource() {
-            console.log(`submit: ${this.st}`)
-            this.tt = await getTrans(this.st)
+            const self = this
+            console.log(`submit: ${self.st}`)
+            console.log(JSON.stringify(self.st))
+            fetch('https://sheepy-meme.builtwithdark.com/mock', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'text/plain',
+                },
+                mode: 'cors',
+                body: JSON.stringify(self.st)
+            }).then(res => {
+                if (res.status === 200) {
+                    res.text().then(t => {
+                        self.tt = `${t}: ${self.st}`
+                    })
+                }
+            })
         }
     },
 })
